ecc0f26a43732194bc698210cb7f131e15bfbd20
paulszh <szh950815@gmail.com>
2018-04-24 01:51:46 -0700

    finish microchallege2

--- ECE 180B/reverseString.cpp
+++ ECE 180B/reverseString.cpp
@@ -1,34 +1,34 @@
-#include <iostream>
+#include<string.h>
+#include<malloc.h>
+#include<iostream>
 
-std::string reverseString(const std::string &str) {
-    std::string result (str.length(), ' ');
-    std::cout << "length : " << result.length() << " " << str.length() << std::endl;
-    // for (int i = 0; i < result.length(); i++) {
-    //     std::cout << str[i] << std::endl;
-    //     result[result.length()-i-1] = str[i]; 
-    // }
-    for (int i = 0, j = result.length() - 1; i <= j; i++, j--) {
+char * reverseString(const char * str) {
+    int len = strlen(str);
+    char *result = (char*)malloc(sizeof(char)*(len+1)); 
+
+    for (int i = 0, j = len - 1; i <= j; i++, j--) {
         result[i] = str[j];
         result[j] = str[i];
     }
-    std::cout << "original string: " << str << "   reversed string:" << result << std::endl;
-    return result; 
+    // std::cout << "original string: " << str << "   reversed string:" << result << std::endl;
+    return result;
 }
 
 int main(int argc, const char * argv[]) {
 
-    std::string test_1 = "";
-    std::string test_2 = " ";
-    std::string test_3 = "zhouhang";
-    std::string test_4 = "abc";
-    std::string test_5 = "sdfdsfdsdsfdsf_sdsafdsfdsf3rwrweefsasfdsfadfasfasfaf093840923849032849080";
-    std::string test_6 = "dsfdsdsfdsf_sdsafdsfdsf3rwrweefsasfdsfadfasfasfaf093840923849032849080";
+    const char * test_1 = "";                    // empty string
+    const char * test_2 = "zhouhang";        // even length string
+    const char * test_3= "abc";             // odd length string
+    // test for a very long string
+    const char * test_4 = "dsfdsdsfdsfsdsafdsfdsf3rwrweefsasfdsfadfasfa.,,.,.sfaf093840923849032849080";
 
-    std::string reverse_1 = reverseString(test_1);
-    std::string reverse_2 = reverseString(test_2);
-    std::string reverse_3 = reverseString(test_3);
-    std::string reverse_4 = reverseString(test_4);
-    std::string reverse_5 = reverseString(test_5);
-    std::string reverse_6 = reverseString(test_6);
-    assert (!reverseString(reverse_5).compare(test_5));
+    char * reverse_1 = reverseString(test_1);
+    char * reverse_2 = reverseString(test_2);
+    char * reverse_3 = reverseString(test_3);
+    char * reverse_4 =  reverseString(test_4);
+
+    std::cout << "original string: " << test_1 << "   reversed string:" << reverse_1 << std::endl;
+    std::cout << "original string: " << test_2 << "   reversed string:" << reverse_2 << std::endl;
+    std::cout << "original string: " << test_3 << "   reversed string:" << reverse_3 << std::endl;
+    std::cout << "original string: " << test_4 << "   reversed string:" << reverse_4 << std::endl;
 }
